323b90036565522858c28514049f6a6acbafd1de
mmontuori <michael.montuori@gmail.com>
2025-05-14 21:19:15 +0000

    install argocd client

--- script/setup-argocd.sh
+++ script/setup-argocd.sh
@@ -2,13 +2,17 @@
 
 minikube kubectl -- create ns argocd
 
-minikube kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.5.8/manifests/install.yaml
+minikube kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.0.1/manifests/install.yaml
 
 echo "waiting for pods to start..."
 
 while minikube kubectl -- get pods -n argocd | awk '{ print $3 }' | grep -v STATUS | grep -v Running > /dev/null; do
     sleep 5
 done
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
 
 argocd_password=`minikube kubectl -- -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo`
 
